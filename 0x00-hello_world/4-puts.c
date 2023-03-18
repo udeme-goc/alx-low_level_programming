f01a037c8ec64e7e0d139c18e68b4b44fcbe3bbd
Udeme Harrison <udeme.goc@gmail.com>
2023-03-18 22:16:21 +0100

    My first C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print line of code using puts function
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
