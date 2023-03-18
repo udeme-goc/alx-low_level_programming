a40e917eb84dba7cf5171e0285cd417db8f96234
Udeme Harrison <udeme.goc@gmail.com>
2023-03-18 23:24:38 +0100

    Hello in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Print line of text using printf
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
