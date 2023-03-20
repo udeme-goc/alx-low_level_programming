59c730bee7a0babbf28efefa9e9af5787db6ff0d
Udeme Harrison <udeme.goc@gmail.com>
2023-03-20 05:34:58 +0100

    C Program printing to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - C program that prints line of text to standard error
+ * Code by Udeme Harrison
+ * Return: 1
+*/
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
