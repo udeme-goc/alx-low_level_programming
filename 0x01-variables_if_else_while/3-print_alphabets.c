b78b6b2effcecf158478cfaf84e167224e342d04
Udeme Harrison <udeme.goc@gmail.com>
2023-03-20 22:10:57 +0100

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,17 +4,23 @@
  * followed by a new line.
  * code by Udeme Harrison
  * Return: 0
-*/
+ */
 
 int main(void)
 {
 	int n = 97;
+	int o = 65;
 
 	while (n <= 122)
 	{
 		putchar(n);
-			n++;
+		n++;
+	}
+	while (o <= 90)
+	{
+		putchar(o);
+			o++;
 	}
 	putchar('\n');
-	return (1);
-:}
+	return (0);
+}
