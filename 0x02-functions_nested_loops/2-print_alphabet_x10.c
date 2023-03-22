4a7a1e3e3906898bdc25e69aec262adacf351e8b
Udeme Harrison <udeme.goc@gmail.com>
2023-03-22 04:13:21 +0100

    10 prints of the lowercase alphabets using _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the lowercase alphabets 10 times.
+ * code by Udeme Harrison
+*/
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char n;
+
+	while (count <= 9, count++)
+	{
+		for (n = 'a'; n <= 'z'; n++)
+			_putchar(n);
+		_putchar('\n');
+	}
+}
