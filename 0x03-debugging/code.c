e7c9e6b482d6b47abfa52dc0e8c06304ccc4c17f
Udeme Harrison <udeme.goc@gmail.com>
2023-03-23 21:25:57 +0100

    Multiple mains

---
+++ 0x03-debugging/code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - return char value
+ * @i: value passed to the function
+ * Return: 0
+ */
+
+int i;
+
+i = 9;
+while (--i)
+{
+	printf("%d", i);
+}
