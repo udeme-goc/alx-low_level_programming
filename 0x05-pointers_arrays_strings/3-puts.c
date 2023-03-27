9513ac7a59ded0e1ba973d7d690bdfabe84a8e7a
Udeme Harrison <udeme.goc@gmail.com>
2023-03-27 20:16:30 +0100

    Print string to standard output

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to standard output
+ * @str: string to print
+ */
+
+void _puts(char *str)
+
+{
+	while (*str != '0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
