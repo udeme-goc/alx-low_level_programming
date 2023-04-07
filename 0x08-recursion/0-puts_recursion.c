c896038f2055009a6e0f1bc07b2092c89821b1f0
Udeme Harrison <udeme.goc@gmail.com>
2023-04-07 22:27:58 +0100

    Recursive function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - output char recursively to standard output with new line.
+ * @s: character string.
+ * code by Udeme Harrison
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is README for 0x08. C - Recursion
