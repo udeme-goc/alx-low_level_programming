7efeef1841aa68001cc315cc4e76a97ea0ff2f78
Udeme Harrison <udeme.goc@gmail.com>
2023-04-09 17:17:48 +0100

    Program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the filename of the program.
+ * @argc: commandline argument counter
+ * @argv: command line vector
+ * code by Udeme Harrison
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (argc);
+}
