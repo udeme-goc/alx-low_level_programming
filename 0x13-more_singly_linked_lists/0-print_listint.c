25063138576157b11bbee7ef45aa3eb625b1c0dd
Udeme Harrison <udeme.goc@gmail.com>
2023-04-28 16:13:46 +0100

    Function that returns the number of elements in a linked listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a linked list
+ * @h: Linked list of type listint_t to print
+ * code adapted by Udeme Harrison
+ * Return: Number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
