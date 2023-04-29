8557c19e715fb89a7d1df5600b4b9aeb1ce8bcf4
udeme-goc <udeme.goc@gmail.com>
2023-04-29 16:45:48 +0100

    Function that prints all the elements of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,28 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a linked list
- * @h: Linked list of type listint_t to print
- * code adapted by Udeme Harrison
- * Return: Number of nodes
+ * print_listint - Prints all the elements of a listint_t list.
+ * @h: A pointer to the head of the linked list.
+ * code by Udeme Harrison
+ * Return: The number of nodes in the list.
  */
 
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t count = 0; /* Init counter vrb to keep track of # of nodes in list. */
 
-	while (h != NULL)
+	while (h != NULL) /* Traverse list until current node is NULL. */
 	{
-		printf("%d\n", h->n);
-		num++;
-		h = h->next;
+		printf("%d", h->n); /* Print the value of the current node. */
+		count++; /* Increment the counter for each node encountered. */
+
+		if (h->next != NULL) /* If current node is not last, print space aft value. */
+		printf(" \n");
+
+		h = h->next; /* Move to the next node in the list. */
 	}
-	return (num);
+
+	printf("\n"); /* Print newline after all nodes have been printed. */
+
+	return (count); /* Return the final count of nodes in the list. */
 }
