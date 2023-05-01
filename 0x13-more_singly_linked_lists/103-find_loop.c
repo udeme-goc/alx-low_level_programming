2ae33d235f65616845b84a33f78e95ba83089d92
udeme-goc <udeme.goc@gmail.com>
2023-05-02 00:29:10 +0100

    Function that finds the loop in a linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -10,27 +10,32 @@
 listint_t *find_listint_loop(listint_t *head)
 {
 	listint_t *slow, *fast;
+
+	if (head == NULL)
 		return (NULL);
 
 	slow = head;
 	fast = head;
 
-	/* move slow pointer one node at a time */
-	slow = slow->next;
-	/* move fast pointer two nodes at a time */
-	fast = fast->next->next;
-	/* if they meet, there is a loop */
-	if (slow == fast)
+	while (slow && fast && fast->next)
 	{
-		/* move slow back to head, keep fast at meeting point */
-		slow = head;
-		while (slow && fast)
+		/* move slow pointer one node at a time */
+		slow = slow->next;
+		/* move fast pointer two nodes at a time */
+		fast = fast->next->next;
+		/* if they meet, there is a loop */
+		if (slow == fast)
 		{
-			/* move both pointers one node at a time */
-			if (slow == fast)
-				return (slow);
-			slow = slow->next;
-			fast = fast->next;
+			/* move slow back to head, keep fast at meeting point */
+			slow = head;
+			while (slow && fast)
+			{
+				/* move both pointers one node at a time */
+				if (slow == fast)
+					return (slow);
+				slow = slow->next;
+				fast = fast->next;
+			}
 		}
 	}
 
