a4178ad64c5a78e9bfd7bf868d7d190332f723d6
Udeme Harrison <udeme.goc@gmail.com>
2023-06-11 22:26:46 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - addition of argument passed in commandline
@@ -25,19 +27,22 @@ int main(int argc, char *argv[])
 	/* argument passed */
 	for (i = 1; i < argc; i++)
 	{
-		x = atoi(argv[i]);
-
-		/* real number check */
-		if (x > 0)
+		/* check if argument is digit */
+		for (j = 0; argv[i][x] != '\0'; x++)
 		{
-			sum += x;
+			if (!isdigit(argv[i][x]))
+			{
+				/* error message for non-digit character */
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		/* convert argument to integer and add it to sum */
+		sum += atoi(argv[i]);
 	}
+	
+	/* print the final sum */
 	printf("%d\n", sum);
+
 	return (0);
 }
