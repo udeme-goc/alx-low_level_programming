34b182bd238fd27278c574771039100bca3ea8c6
Udeme Harrison <udeme-goc@gmail.com>
2023-07-14 00:52:49 +0100

    Function that appends text to the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,38 +1,85 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text to the end of a file.
- * @filename: The name of the file.
- * @text_content: The text content to append.
+ * close_file - Closes an opened file.
+ * @FD: File descriptor to close.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: None.
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+void close_file(int FD)
 {
-	int fd, bytes_written;
-	size_t text_len = 0;
-
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content != NULL)
+	/* Close the file descriptor */
+	if (close(FD) == -1)
 	{
-		while (text_content[text_len])
-			text_len++;
+		/* Error occurred while closing file descriptor */
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", FD);
+		exit(100);
+	}
+}
+
+
+/**
+ * main - Entry point.
+ * @argc: Number of arguments passed to the function.
+ * @argv: Two files.
+ *
+ * Return: 0 on success, other values on failure.
+ */
+
+int main(int argc, char *argv[])
+{
+	int inputFD, outputFD, nBytes_read, nBytes_write;
+	char text[BUF_SIZE];
+
+	/* Check if the correct number of arguments is provided */
+	if (argc != 3)
+	{
+		/* Incorrect number of arguments, display error message */
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-
-	bytes_written = write(fd, text_content, text_len);
-	if (bytes_written == -1)
+	/* Open the input file for reading */
+	inputFD = open(argv[1], O_RDONLY);
+	if (inputFD == -1)
 	{
-		close(fd);
-		return (-1);
+		/* Error opening the input file, display error message */
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
-	close(fd);
-	return (1);
+	/* Open the output file for writing, create if it doesn't exist */
+	outputFD = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (outputFD == -1)
+	{
+		/* Error opening the output file, display error message */
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	/* Read data from the input file and write to the output file */
+	while ((nBytes_read = read(inputFD, text, BUF_SIZE)) > 0)
+	{
+		nBytes_write = write(outputFD, text, nBytes_read);
+		if (nBytes_write == -1)
+		{
+			/* Error occurred while writing to the output file, display error message */
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	if (nBytes_read == -1)
+	{
+		/* Error occurred while reading from the input file, display error message */
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	/* Close the input and output files */
+	close_file(inputFD);
+	close_file(outputFD);
+
+	return (0);
 }
